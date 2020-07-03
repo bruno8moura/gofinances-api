81ef92e87520a3946417df971b6c40df85eea4fd
Bruno Moura <bruno8moura@gmail.com>
2020-07-03 06:20:44 -0300

    Delete a transaction.

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -3,7 +3,7 @@ import { getCustomRepository } from 'typeorm';
 
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import CreateTransactionService from '../services/CreateTransactionService';
-// import DeleteTransactionService from '../services/DeleteTransactionService';
+import DeleteTransactionService from '../services/DeleteTransactionService';
 // import ImportTransactionsService from '../services/ImportTransactionsService';
 
 const transactionsRouter = Router();
@@ -31,7 +31,12 @@ transactionsRouter.post('/', async (request, response) => {
 });
 
 transactionsRouter.delete('/:id', async (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const service = new DeleteTransactionService();
+  await service.execute({ id });
+
+  return response.status(204).json().end();
 });
 
 transactionsRouter.post('/import', async (request, response) => {
--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,30 @@
 // import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import Transaction from '../models/Transaction';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import AppError from '../errors/AppError';
 
+interface Request {
+  id: string;
+}
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute({ id }: Request): Promise<void> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+    const transaction = await transactionsRepository.findOne({ id });
+
+    if (!transaction) {
+      throw new AppError(
+        'Is not possible delete the Transaction because it does not exit',
+      );
+    }
+
+    await transactionsRepository
+      .createQueryBuilder()
+      .delete()
+      .from(Transaction)
+      .where('id = :id', { id })
+      .execute();
   }
 }
 
